ad80f8942f69927309360d95d8623a1945c5cff3
jutikapatil <jutikapatil123@gmail.com>
2024-02-14 00:30:44 +0530

    fixed key error

--- components/Post.tsx
+++ components/Post.tsx
@@ -73,7 +73,7 @@ export default async function Post(
         <span><Image className=" m-2" src={'/link.svg'} alt={"link-logo"} height={20} width={20}/></span>
         {links && (
         <div className="overflow-hidden mx-4 ">
-          {links.map((link , i)=>(<div><Link href={link}>{link}</Link></div>))}
+          {links.map((link)=>(<div><Link href={link}>{link}</Link></div>))}
         </div>
         )}
 
@@ -84,7 +84,7 @@ export default async function Post(
       {
         isEditable && (
           <div className="flex gap-4" >
-            <Link className="  text-dark bg-slate-50" href={`edit-post/${id}`}>Edit</Link>
+            <Link  className="  text-dark bg-slate-50" href={`edit-post/${id}`}>Edit</Link>
             <DeleteButton id={id} />
           </div>
         )
